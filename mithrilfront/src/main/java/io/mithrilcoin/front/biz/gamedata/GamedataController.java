f7a4cc7c98594e6c6d31ff541652a2ab8836992d
Kei <Kei@DESKTOP-HJ4IFLC>
2018-01-24 20:51:36 +0900

    데모용 Nopaging 게임 데이터 조회 함수 추가.

--- mithrilfront/src/main/java/io/mithrilcoin/front/biz/gamedata/GamedataController.java
+++ mithrilfront/src/main/java/io/mithrilcoin/front/biz/gamedata/GamedataController.java
@@ -1,5 +1,6 @@
 package io.mithrilcoin.front.biz.gamedata;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.mithril.vo.member.UserInfo;
+import io.mithril.vo.playdata.PlayData;
 import io.mithril.vo.playdata.Playstoreappinfo;
 import io.mithril.vo.playdata.TemporalPlayData;
 import io.mithrilcoin.front.common.redis.RedisDataRepository;
@@ -84,6 +87,18 @@ public class GamedataController {
 		TemporalPlayData result = mithrilApiTemplate.post("/gamedata/insert/reward/" + email, playdataParam, typeRef);
 
 		return new MithrilResponseEntity<TemporalPlayData>(result, HttpStatus.OK, id, userRedisSessionInfo);
+	}
+
+	@GetMapping("/select/nopage/{id}")
+	public MithrilResponseEntity<ArrayList<PlayData>> selectPlayDataNopage(@PathVariable String id) throws Exception {
+		String email = redisdataRepo.getData("email_" + id);
+		email = URLEncoder.encode(email, "UTF-8");
+		ParameterizedTypeReference<ArrayList<PlayData>> typeRef = new ParameterizedTypeReference<ArrayList<PlayData>>() {
+		};
+		ArrayList<PlayData> playdataList = mithrilApiTemplate.get("/gamedata/select/nopage/" + email, "", typeRef);
+
+		return new MithrilResponseEntity<ArrayList<PlayData>>(playdataList, HttpStatus.OK, id, userRedisSessionInfo);
+
 	}
 
 }
